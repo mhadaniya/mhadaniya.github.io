00890d17e2e49dd2cb020ee6fabc87a7f67a5910
Mario H. Adaniya (mhadaniya) <mhadaniya@gmail.com>
2023-01-30 20:37:50 -0300

    :wrench: tailwindcss config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/tailwind.css"],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
   build: {
     postcss: {
       postcssOptions: {
