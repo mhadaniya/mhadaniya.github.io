6b3880285deb2a0c3604826043b856d795d8dd99
Mario H. Adaniya <mhadaniya@gmail.com>
2023-01-30 16:31:22 -0300

    🔥 started all over again

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
